654ac24eb4617646ccabec2a5c474f53fafff859
zipang <christophe.Desguez@gmail.com>
2021-12-01 17:06:51 +0100

    Update the JSDoc and pass moreProps to the Box

--- src/components/base/Typography.js
+++ src/components/base/Typography.js
@@ -12,10 +12,17 @@ export const Title = ({ children, ...moreProps }) => (
 
 /**
  * Display some code result inside a grey box
- * @param {Object} propsWithChildren
+ * @param {Object} props
+ * @param {Boolean} [props.error=false] Pass true to surround with a red border
+ * @param {JSX.Element} props.children The things to display
  */
 export const CodeBox = ({ children, error = false, ...moreProps }) => (
-	<Box p={5} bgColor="#f2f5f7" border={error ? "2px dashed red" : "2px solid #c3cfd9"}>
+	<Box
+		p={5}
+		bgColor="#f2f5f7"
+		border={error ? "2px dashed red" : "2px solid #c3cfd9"}
+		{...moreProps}
+	>
 		<code>{children}</code>
 	</Box>
 );
