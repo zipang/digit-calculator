72a2ee531cb7e1d411eae81cdbb5175b715a8dd8
zipang <christophe.Desguez@gmail.com>
2021-12-01 16:10:25 +0100

    Basically we are done now, we just have to improve everything

--- src/components/DigitTest.js
+++ src/components/DigitTest.js
@@ -1,11 +1,14 @@
 import { Box } from "@chakra-ui/layout";
+import { Select } from "@chakra-ui/select";
 import { useEffect, useState } from "react";
 import { Title } from "./base/Typography";
 import { TagList } from "./TagList";
 
+const availableDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const DigitTest = () => {
-	// We must store the selected digits
-	const [selectedDigits, setSelectedDigits] = useState([1]);
+	// We store the selected digits
+	const [selectedDigits, setSelectedDigits] = useState([]);
 	const [sum, updateSum] = useState(0);
 
 	// Anytime the selection is updated, we must update the sum
@@ -13,12 +16,17 @@ const DigitTest = () => {
 		updateSum(selectedDigits.reduce((sum, d) => sum + d, 0));
 	}, [selectedDigits]);
 
-	// Utility method to add and remove digits from the selection
-	const addDigit = (d) => {
+	// Utility methods to add and remove valid digits from the selection
+	const isSelected = (d) => selectedDigits.find((x) => x === d);
+	const addDigit = (evt) => {
 		try {
-			const digitValue = Number.parseInt(d, 10); // It must be an integer
-			if (!selectedDigits.find((x) => x === digitValue)) {
-				setSelectedDigits([digitValue, ...selectedDigits]);
+			const digitValue = Number.parseInt(evt.target.value, 10); // It must be an integer
+			if (digitValue && !isSelected(digitValue)) {
+				// NaN is excluded here
+				setSelectedDigits(
+					// Add the new digit and re-sort the array
+					[digitValue, ...selectedDigits].sort((a, b) => (a > b ? 1 : -1))
+				);
 			}
 		} catch (err) {
 			console.error(`addDigit(${d}) failed`, error);
@@ -36,6 +44,13 @@ const DigitTest = () => {
 	return (
 		<Box className="digit-sum-calculator">
 			<Title>Select Digits to create a sum</Title>
+			<Select placeholder="Select a digit" onChange={addDigit}>
+				{availableDigits.map((d) => (
+					<option key={d} value={d} disabled={isSelected(d)}>
+						{d}
+					</option>
+				))}
+			</Select>
 
 			<Title as="h3">Sum</Title>
 			<code>{sum}</code>
